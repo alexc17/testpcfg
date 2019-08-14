871bfe29b09169bd7d11b04fd4df8dca23287282
alexc <alexsclark@gmail.com>
2019-08-14 15:39:29 +0100

    adding makefile again

--- scripts/generate_data.sh
+++ scripts/generate_data.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 seed=1
-n=2
+n=100
+lex=1000
+lr=1000
+nt=10
 sampler="../../syntheticpcfg/syntheticpcfg/sample_grammar.py"
 for br in 20 30 40 50 60 70 80
 do
     direct="../data/test$br"
     mkdir $direct
-    python $sampler --seed $seed --numbergrammars $n --binaryproductions $br "${direct}/grammar%d.pcfg"
+    python $sampler --seed $seed --nonterminals $nt --numbergrammars $n --lexicalproductions $lr --terminals $lex --binaryproductions $br "${direct}/grammar%d.pcfg"
 done
 echo "Done"
